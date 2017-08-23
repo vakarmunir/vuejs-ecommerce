0dd4201188b5e7f00d9c1be3797e226c6aa3cc2d
Waqar Muneer <2662505+waqarmuneer@users.noreply.github.com>
2017-08-23 12:12:27 +0500

    google material vuetify theme implementation
    google material vuetify theme implementation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="app" class="container">
     <div class="row">
       <div class="col">
@@ -6,20 +6,18 @@
       </div>
     </div>
     <div class="row show-grid">
-      <!-- Left Nav -->
       <div class="col col-md-2">
         <div class="row">
           <div class="col">
             <b-nav pills vertical>
               <b-nav-item to="/" exact>Products</b-nav-item>
               <b-nav-item to="/add-new">Add New</b-nav-item>
-              <!-- <b-nav-item>Another Link</b-nav-item>
-              <b-nav-item disabled>Disabled</b-nav-item> -->
+
             </b-nav>
           </div>
         </div>
       </div>
-      <!-- Main Body -->
+
       <div class="col col-md-10">
         <div class="row">
           <div class="col">
@@ -30,6 +28,68 @@
     </div>
 
   </div>
+</template> -->
+<template>
+  <v-app
+    height="415px"
+    light
+    id="e3"
+    standalone
+  >
+    <v-navigation-drawer
+      class="pb-0"
+      persistent
+      absolute
+      height="100%"
+      clipped
+      enable-resize-watcher
+      v-model="drawer"
+    >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text" :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="indigo" dark>
+      <v-toolbar-title>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <!-- <v-icon class="ml-3">fa-youtube</v-icon> -->
+        Vue App
+      </v-toolbar-title>
+
+    </v-toolbar>
+    <main>
+      <v-container>
+        <v-layout>
+          <v-flex xs12>
+            <v-card>
+              <v-card-text>
+                <v-container fluid>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-content>
+                        <router-view></router-view>
+                      </v-content>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+            </v-card>
+
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
@@ -39,7 +99,11 @@ export default {
   components: {Tabs},
   data () {
     return {
-
+      drawer: true,
+       items: [
+         { icon: 'dashboard', text: 'Home' , link: "/" },
+         { icon: 'add_box', text: 'Add Product' , link: '/add-new' }
+       ]
     }
   },
   created()
